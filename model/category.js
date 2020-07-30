acb6554f856e559c7271dfd8d5b91271c3812488
mintsberry <mintberfly@gmail.com>
2020-07-30 21:57:57 +0800

    refactor: 主题api请求

--- model/category.js
+++ model/category.js
@@ -1,7 +1,7 @@
 import { Http } from "../utils/http";
 
 class Category {
-  static async getGridCategory() {
+  static async getHomeLocationC() {
     const url = '/category/grid/all'
     return await Http.reqeust(url)
   }
--- model/theme.js
+++ model/theme.js
@@ -1,10 +1,32 @@
 import { Http } from "../utils/http";
 
 export class Theme {
-  static async getHomeLocationA(callback) {
+  static locationA = 't-1'
+  static locationE = 't-2'
+  static locationF = 't-3'
+  static locationH = 't-4'
+
+  themes = []
+
+  async getThemeGroup() {
+    const names = `${Theme.locationA},${Theme.locationE},${Theme.locationF},${Theme.locationH}`
     const url = `/theme/by/names`
-    return await Http.reqeust(url, {
-      names: 't-1'
-    } )
+    this.themes =(await Http.reqeust(url, {
+      names
+    })).data
+    console.log(this.themes)
+  }
+
+  getHomeLocationA() {
+    return this.themes.find(el => el.name === Theme.locationA)
+  }
+  getHomeLocationE() {
+    return this.themes.find(el => el.name === Theme.locationE)
+  }
+  getHomeLocationF() {
+    return this.themes.find(el => el.name === Theme.locationF)
+  }
+  getHomeLocationH() {
+    return this.themes.find(el => el.name === Theme.locationH)
   }
 }
